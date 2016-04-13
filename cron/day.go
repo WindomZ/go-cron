e2c2d1dc08a6d0b8cc8d15ebc2e74f78a7ad62a7
WindomZ <zwdsdtc@gmail.com>
2016-04-13 16:02:04 +0800

    feat: more day functions

--- cron/day.go
+++ cron/day.go
@@ -1,6 +1,7 @@
 package cron
 
 import (
+	"fmt"
 	. "github.com/robfig/cron"
 	"time"
 )
@@ -16,4 +17,23 @@ func EveryDays(d int, cmd func()) error {
 func EveryDay(cmd func()) error {
 	getCron().AddFunc("0 0 0 * * *", FuncJob(cmd))
 	return nil
+}
+
+func EveryFixedDay(start int, interval int, cmd func()) error {
+	if err := ValidHour(start); err != nil {
+		return ErrValueStart
+	} else if err := ValidHour(interval); err != nil {
+		return ErrValueInterval
+	} else if err := ValidHour(start + interval); err != nil {
+		return err
+	}
+	return getCron().AddFunc(fmt.Sprintf("* * %v/%v * * *", start, interval), FuncJob(cmd))
+}
+
+func EveryHalfDay(cmd func()) error {
+	return EveryFixedDay(0, 30, cmd)
+}
+
+func EveryQuarterDay(cmd func()) error {
+	return EveryFixedDay(0, 15, cmd)
 }
--- cron/func.go
+++ cron/func.go
@@ -4,6 +4,10 @@ func range0To59(n int) bool {
 	return (n >= 0 && n <= 59)
 }
 
+func range0To23(n int) bool {
+	return (n >= 0 && n <= 23)
+}
+
 func ValidSecond(n int) error {
 	if !range0To59(n) {
 		return ErrSecond
@@ -13,6 +17,13 @@ func ValidSecond(n int) error {
 
 func ValidMinute(n int) error {
 	if !range0To59(n) {
+		return ErrMinute
+	}
+	return nil
+}
+
+func ValidHour(n int) error {
+	if !range0To23(n) {
 		return ErrMinute
 	}
 	return nil
